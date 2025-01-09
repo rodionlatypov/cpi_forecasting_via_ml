b5f3dd32edef6db94f2b404f175ee9e3db6ebb6a
rodionlatypov <71976084+rodionlatypov@users.noreply.github.com>
2025-01-09 21:37:22 +0300

    Add files via upload

---
+++ lib/etna_utils/univariate_models/univariate_models_utils.py
@@ -0,0 +1,16 @@
+import itertools
+import pandas as pd
+import typing as tp
+
+import etna
+import mpire
+
+from . import univariate_models_args
+
+
+# def get_model_a
+
+
+from etna.pipeline import Pipeline
+
+# def 
